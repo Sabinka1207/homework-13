67b1996e08beab3e3a127cf3462dfb31c10256f4
Sabina Abdulaliieva <77077884+Sabinka1207@users.noreply.github.com>
2021-08-03 11:14:52 +0300

    async fetch added

--- src/js/postAPI.js
+++ src/js/postAPI.js
@@ -19,7 +19,7 @@ export default class PostAPI {
         this.searchQuery = newQuery
     }
 
-    fetchPosts() {
+    async fetchPosts() {
         const params = new URLSearchParams({
         key: '16347216-fb50075837ded8f23f3479d31',
         q: this.searchQuery,
@@ -30,17 +30,19 @@ export default class PostAPI {
         per_page: perPage
         })
 
-    return axios.get(`${url}?${params}`).then(r => {
+    return await axios.get(`${url}?${params}`).then(r => {
         if (r.data.total === 0) {
             onLoadMore.classList.add('is-hidden')
             return Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
         } else {
             console.log(r.data, this.page, r.data.totalHits/perPage)
             onLoadMore.classList.remove('is-hidden')
-            if (this.page > r.data.totalHits/perPage+1) {
+            
+            if (this.page > r.data.totalHits / perPage + 1) {
             onLoadMore.classList.add('is-hidden')
             return Notiflix.Notify.warning("We're sorry, but you've reached the end of search results")
             }
+            
             this.page += 1
             return r.data.hits
         }
